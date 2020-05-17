7eccda34be505ff4da75e6f4ad6a5b9eca30b0b2
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-17 18:24:34 -0400

    [TASK] Clear grid 2, remove unnecessary function

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -54,23 +54,6 @@ void print_grid(int grid[3][3])
 }
 
 /**
- * stabilize_sandpile - Checks if its not stable
- * @new_grid: grid to stabilize into a sandpile
- */
-void stabilize_sandpile(int new_grid[3][3])
-{
-	int i = 0, j = 0;
-
-	for (; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			if (new_grid[i][j] > 3)
-			{
-				print_grid(new_grid);
-				fix_sandpile(new_grid);
-			}
-}
-
-/**
  * sandpiles_sum - Calculaes the sum of two sandpiles
  * @grid1: double array (sandpile)
  * @grid2: double array (sandpile)
@@ -81,7 +64,16 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
 	for (; i < 3; i++)
 		for (j = 0; j < 3; j++)
+		{
 			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
 
-	stabilize_sandpile(grid1);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid1[i][j] > 3)
+			{
+				print_grid(grid1);
+				fix_sandpile(grid1);
+			}
 }
