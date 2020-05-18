13ebf2430f4d6c450573b51d6b0a946d9ecc3a7c
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-18 10:32:29 -0400

    Add files

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * count_nodes - counts the number of nodes inside the tree.
+ * @tree: binary tree node.
+ * Return: number of nodes in the tree.
+ */
+int count_nodes(heap_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
+}
+
+/**
+ * is_complete - function that checks if tree is complete.
+ * @tree: binary tree node.
+ * @index: index of the tree.
+ * @n: number of nodes.
+ * Return: 1 on success 0 on failure.
+ */
+heap_t *is_complete(heap_t *tree, int index, int n)
+{
+	heap_t *left, *right;
+
+	if ((tree->left) && !(tree->right))
+		return (tree);
+	if (!(tree->left) && !(tree->right))
+	{
+		if (index == n)
+			return (NULL);
+		else
+			return (tree);
+	}
+
+	left = is_complete(tree->left, (2 * index) + 1, n);
+	right = is_complete(tree->right, (2 * index) + 2, n);
+
+	if  (left)
+		return (left);
+	else
+	    return (right);
+}
+
+/**
+ * binary_tree_node - creates a binary tree
+ *
+ *
+ * @parent: parent of tree
+ * @value: value for tree
+ * Return: Always 0 (Success)
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *new;
+	heap_t *parent;
+	int size;
+
+	new = malloc(sizeof(heap_t));
+	
+	if (!new)
+		return (NULL);
+	parent = malloc(sizeof(heap_t));
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+
+	if (!*root)
+	{
+		*root = new;
+		return (new);
+	}
+
+	size = count_nodes(*root);
+	parent = is_complete(*root, 0, size);
+	if (!parent->left)
+	{
+		parent->left = new;
+		new->parent = parent;
+	}
+	else
+	{
+		parent->right = new;
+		new->parent = parent;
+	}
+	return (new);
+}
