ce010660fe69fd33cfd676f508253c60965d5370
Jose Alvarez de Lugo <josealvarezdelugo@Joses-MacBook-Pro.local>
2020-05-26 17:22:39 -0400

    [TASK] Check if linked list is palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -10,33 +10,42 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *rev, *prev = NULL, *copy = *head;
+    listint_t *rev, *prev = NULL, *copy = *head, *hold;
     int size;
 
-    if (!head || !*head)
+    if (!head)
         return (0);
+    if (!*head)
+        return (1);
 
     for (size = 0; copy; size++)
     {
         rev = malloc(sizeof(listint_t));
+        if (!rev)
+            return (0);
         rev->n = copy->n;
         rev->next = prev;
         prev = rev;
         copy = copy->next;
     }
-    
+
+    hold = rev;
     copy = *head;
     size /= 2;
 
     for (; size; size--)
     {
         if (rev && copy && rev->n != copy->n)
+        {
+            free_listint(hold);
             return (0);
+        }
         else
         {
             rev = rev->next;
             copy = copy->next;
         }
     }
+    free_listint(hold);
     return (1);
 }
