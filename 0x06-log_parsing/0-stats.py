5ac910cd16713e3eeea4510f460f9a720dd36245
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-31 16:10:55 -0400

    [TASK] Read stdin line by line and compute metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,30 +3,31 @@
 import sys
 
 
-status = {"200": 0,
-          "301": 0,
-          "400": 0,
-          "401": 0,
-          "403": 0,
-          "404": 0,
-          "405": 0,
-          "500": 0}
-count = 0
-file_size = 0
-try:
-    for line in sys.stdin:
-        count += 1
-        status_code = line.split()[-2]
-        file_size += int(line.split()[-1])
-        status[status_code] += 1
-        if count % 10 == 0:
-            print("File size:", file_size)
-            for key, value in status.items():
-                if value:
-                    print(key + ": " + str(value))
-except KeyboardInterrupt:
-    print("File size:", file_size)
-    for key, value in status.items():
-        if value:
-            print(key + ": " + str(value))
-    raise(KeyboardInterrupt)
+if __name__ == "__main__":
+    status = {"200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0}
+    count = 0
+    file_size = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            status_code = line.split()[-2]
+            file_size += int(line.split()[-1])
+            status[status_code] += 1
+            if count % 10 == 0:
+                print("File size:", file_size)
+                for key, value in status.items():
+                    if value:
+                        print(key + ": " + str(value))
+    except KeyboardInterrupt:
+        print("File size:", file_size)
+        for key, value in status.items():
+            if value:
+                print(key + ": " + str(value))
+        raise(KeyboardInterrupt)
