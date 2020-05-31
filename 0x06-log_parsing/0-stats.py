ddaf32fc07371456d5e1a8bb10158c2769f408d2
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-05-31 15:59:11 -0400

    [TASK] Read stdin line by line and compute metrics

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+import sys
+
+
+status = {"200": 0,
+          "301": 0,
+          "400": 0,
+          "401": 0,
+          "403": 0,
+          "404": 0,
+          "405": 0,
+          "500": 0}
+count = 0
+file_size = 0
+try:
+    for line in sys.stdin:
+        count += 1
+        status_code = line.split()[-2]
+        file_size += int(line.split()[-1])
+        status[status_code] += 1
+        if count > 9:
+            print("File size:", file_size)
+            for key, value in status.items():
+                if value:
+                    print(key + ": " + str(value))
+except KeyboardInterrupt:
+    print("File size:", file_size)
+    for key, value in status.items():
+        if value:
+            print(key + ": " + str(value))
+    raise(KeyboardInterrupt)
