daa12cc4e0247d696da4f3703272ecb3e96a523e
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-06-30 18:25:02 -0400

    [TASK] Slide list on ints to left or right like the game 2048

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,94 @@
+#include "slide_line.h"
+#include <stdio.h>
+
+/**
+ * go_left - pushes line left
+ * @line: list of integers
+ * @size: size of list
+ */
+void go_left(int *line, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (line[i] == 0 && line[j] != 0)
+			{
+				line[i] = line[j];
+				line[j] = 0;
+				i--;
+				break;
+			}
+			if (line[i] == line[j] && line[i] != 0)
+			{
+				line[i] *= 2;
+				line[j] = 0;
+				break;
+			}
+			else if (line[j] != 0 && line[i + 1] == 0)
+			{
+				line[i + 1] = line[j];
+				line[j] = 0;
+				break;
+			}
+		}
+	}
+}
+
+/**
+ * go_right - pushes line right
+ * @line: list of integers
+ * @size: size of list
+ */
+void go_right(int *line, size_t size)
+{
+	size_t i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i + 1; j <= size; j++)
+		{
+			if (line[size - i] == 0 && line[size - j] != 0)
+			{
+				line[size - i] = line[size - j];
+				line[size - j] = 0;
+				i--;
+				break;
+			}
+			if (line[size - i] == line[size - j] && line[size - i] != 0)
+			{
+				line[size - i] *= 2;
+				line[size - j] = 0;
+				break;
+			}
+			else if (line[size - j] != 0 && line[size - (i + 1)] == 0)
+			{
+				line[size - (i + 1)] = line[size - j];
+				line[size - j] = 0;
+				break;
+			}
+		}
+	}
+}
+/**
+ * slide_line - slides line to left or right
+ * @line: list of numbers to slide
+ * @size: size of list
+ * @direction: direction to slide
+ *
+ * Return: 1 if successful or 0 if fails
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	if (!line || !size)
+		return (0);
+	if (direction == 76)
+		go_left(line, size);
+	else if (direction == 82)
+		go_right(line, size);
+	else
+		return (0);
+	return (1);
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef SLIDELINE
+#define SLIDELINE
+
+#include <stddef.h>
+
+#define SLIDE_LEFT 76
+#define SLIDE_RIGHT 82
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /*SLIDELINE*/
