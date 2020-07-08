2637909d2a981c5d2b656293d26b4a6840b26b8f
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-07-08 16:23:18 -0400

    [TASK] Print menger sponge of size N

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,35 +1,41 @@
 #include "menger.h"
-#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * find_hole - finds if it should print # or hole
+ * @i: row that we are on
+ * @j: column that we are on
+ * @check: integer used for logic
+ * Return: # if no hole, or ' ' if hole
+ */
+char find_hole(int i, int j, int check)
+{
+	if (check == 0)
+		return ('#');
+	if ((i % (check * 3)) / check == 1)
+		if ((j % (check * 3)) / check == 1)
+			return (' ');
+	return (find_hole(i, j, check / 3));
+}
+
 
 /**
  * menger - prints a 2D menger sponge
  * @level: level of menger sponge
- * 
+ *
  */
 void menger(int level)
 {
-    int N = pow(3, level), size, i, **layers, j;
+	int i, j, levelSize;
 
-    size = pow(N, 2);
-    layers = malloc(sizeof(int) * (N + 1));
+	levelSize = pow(3, level);
 
-    for (i = 0; i < N; i++)
-    {
-        *layers = malloc(sizeof(int) * (N + 1));
-        for (j = 0; j < N; j++)
-        {
-            layers[i][j] = 1;
-        }
-    }
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            if (layers[i][j])
-                printf("#");
-            else
-                printf(" ");
-        }
-        printf("\n");
-    }
+	for (i = 0; i < levelSize; i++)
+	{
+		for (j = 0; j < levelSize; j++)
+		{
+			putchar(find_hole(i, j, levelSize / 3));
+		}
+		putchar('\n');
+	}
 }
--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,7 +1,6 @@
 #ifndef MENGER
 #define MENGER
 
-#include <stdio.h>
 #include <math.h>
 
 void menger(int level);
