91bf7f9e330a8a6455d6128a7dfc918c718cd770
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-10-07 11:20:52 -0400

    [TASK] Rotate 2D matrix 90 degrees in place

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,16 +7,8 @@ def rotate_2d_matrix(matrix):
     *you can assume the matrix will have 2 dims and not empty
     *Do not return anything. Matrix must be edited in place
     """
-    N = len(matrix)
-    for i in range(int(N / 2)):
-        for j in range(N - i - 1):
-            temp = matrix[i][j]
-            # print(f"[{i}][{j}] <- [{N-1-j}][{i}]")
-            matrix[i][j] = matrix[N-1-j][i]
-            # print(f"[{N-1-j}][{i}] <- [{N-1-i}][{N-1-j}]")
-            matrix[N-1-j][i] = matrix[N-1-i][N-1-j]
-            # print(f"[{N-1-i}][{N-1-j}] <- [{j}][{N-1-i}]")
-            matrix[N-1-i][N-1-j] = matrix[j][N-1-i]
-            # print(f"[{j}][{N-1-i}] <- [{i}][{j}]")
-            matrix[j][N-1-i] = temp
-            # print("----------")
+    matrix.reverse()
+    matrix_temp = matrix.copy()
+
+    for i in range(len(matrix)):
+        matrix[i] = [row[i] for row in matrix_temp]
