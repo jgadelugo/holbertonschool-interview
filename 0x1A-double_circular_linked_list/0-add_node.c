c403b502ffbfeec63cb1da97bec5a8f491a36237
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-11-18 14:36:19 -0500

    [TASK] Add node to the beginning and end of a circular double linked list

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,70 @@
+#include "list.h"
+#include <stdlib.h>
+
+
+/**
+ * add_node_end - Adds a node to the end
+ * @list: the list to modify
+ * @str: string to copy into the new node
+ * Return: address of the new node or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+
+	/* create new node */
+	new = malloc(sizeof(List *));
+	if (!new)
+		return (NULL);
+	new->str = str;
+	new->prev = new;
+	new->next = new;
+
+	if (!list || !*list)
+	{
+		*list = new;
+		return (new);
+	}
+
+	new->prev = (*list)->prev;
+	new->prev->next = new;
+	(*list)->prev = new;
+	new->next = *list;
+
+	return (new);
+}
+
+
+/**
+ * add_node_begin - adds node at beginning
+ * @list: the list to modify
+ * @str: string to copy into the new node
+ * Return: address of the new node or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	/* create new node */
+	new = malloc(sizeof(List *));
+	if (!new)
+		return (NULL);
+	new->str = str;
+	new->prev = new;
+	new->next = new;
+
+	if (!list || !*list)
+	{
+		*list = new;
+		return (new);
+	}
+
+	new->prev = (*list)->prev;
+	new->prev->next = new;
+	(*list)->prev = new;
+	new->next = *list;
+
+	*list = new;
+
+	return (new);
+}
