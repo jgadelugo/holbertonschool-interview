b691b39b856cc508f8dafbef1a30fe60c58b1c66
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-12-02 18:43:21 -0500

    [BUGFIX] Fix counting sort in radix sort

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,12 +1,13 @@
- #include "sort.h"
+#include "sort.h"
 
 /**
  * radix_lsd - sort an array using radix sort lsd
  * @array: array to sort
  * @size: size of array
  * @div: divisor
+ * @minNum: smallest number in list
  */
-void radix_lsd(int *array, int size, int div)
+void radix_lsd(int *array, int size, int div, int minNum)
 {
 	int i;
 	int bucket[10] = {0}, bucketIDX;
@@ -16,16 +17,15 @@ void radix_lsd(int *array, int size, int div)
 	if (!output)
 		return;
 	for (i = 0; i < size; i++)
-		bucket[array[i] / div % 10]++;
+		bucket[(array[i] - minNum) / div % 10]++;
 
-	for (i = 1; i < size; i++)
+	for (i = 1; i < 10; i++)
 		bucket[i] += bucket[i - 1];
 
 	for (i = size - 1; i >= 0; i--)
 	{
-		bucketIDX = array[i] / div % 10;
-		output[bucket[bucketIDX] - 1] = array[i];
-		bucket[bucketIDX]--;
+		bucketIDX = (array[i] - minNum) / div % 10;
+		output[--bucket[bucketIDX]] = array[i];
 	}
 	for (i = 0; i < size; i++)
 		array[i] = output[i];
@@ -40,18 +40,23 @@ void radix_lsd(int *array, int size, int div)
 void radix_sort(int *array, size_t size)
 {
 	size_t i;
-	int maxNum;
+	int maxNum, minNum;
 
 	if (!array || !size || size <= 1)
 		return;
 
 	maxNum = array[0];
+	minNum = array[0];
 	for (i = 1; i < size; i++)
+	{
 		if (array[i] > maxNum)
 			maxNum = array[i];
-	for (i = 1; maxNum / i > 0; i *= 10)
+		if (array[i] < minNum)
+			minNum = array[i];
+	}
+	for (i = 1; (maxNum - minNum) / i > 0; i *= 10)
 	{
-		radix_lsd(array, size, i);
+		radix_lsd(array, size, i, minNum);
 		print_array(array, size);
 	}
 }
