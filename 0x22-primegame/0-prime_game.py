209dfddae46995d6cc3d2904ddf56368c7b72f20
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2021-01-30 10:55:23 -0500

    [TASK] Add function that returns winner of prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+Returns winner of prime game
+
+Prime Game:
+    Maria and Ben are playing a game. Given a set of consecutive integers
+    starting from 1 up to and including n, take turns choosing a prime
+    number from the set and removing that number and its multiples from
+    set. The player that cannot make a move loses the game.
+
+    They play x rounds, where n may be different for each round
+    Assuming Maria always goes first and both players play optimally,
+    determine who the winner of each game is.
+"""
 
 
 def pop_mults(p, mults):
